74c97e493e0386aa7a8350c0f2e4a3fd1fc33716
yy-gx <yyang9923@gmail.com>
2024-11-04 16:06:58 -0500

    regenerate dataset noop

--- experiments/robot/libero/run_libero44_eval.py
+++ experiments/robot/libero/run_libero44_eval.py
@@ -72,8 +72,8 @@ class GenerateConfig:
     #################################################################################################################
     # LIBERO environment-specific parameters
     #################################################################################################################
-    # task_suite_name: str = "libero_90"          # Task suite. Options: libero_spatial, libero_object, libero_goal, libero_10, libero_90
-    task_suite_name: str = "single_step"  # Task suite. Options: libero_spatial, libero_object, libero_goal, libero_10, libero_90
+    task_suite_name: str = "libero_90"          # Task suite. Options: libero_spatial, libero_object, libero_goal, libero_10, libero_90
+    # task_suite_name: str = "single_step"  # Task suite. Options: libero_spatial, libero_object, libero_goal, libero_10, libero_90
     num_steps_wait: int = 5                         # Number of steps to wait for objects to stabilize in sim
     num_trials_per_task: int = 20                    # Number of rollouts per task
 
@@ -268,7 +268,7 @@ def eval_libero(cfg: GenerateConfig) -> None:
         print(f"Current task success rate: {float(task_successes) / float(task_episodes)}")
         print(f"Current total success rate: {float(total_successes) / float(total_episodes)}")
         tasks_success_list.append(float(task_successes) / float(task_episodes))
-        np.save(os.path.join(cfg.local_log_dir, "tasks_success_list.npy"), np.array(tasks_success_list))
+        np.save(os.path.join(cfg.local_log_dir, f"tasks_success_list_{cfg.task_suite_name}.npy"), np.array(tasks_success_list))
         log_file.write(f"Current task success rate: {float(task_successes) / float(task_episodes)}\n")
         log_file.write(f"Current total success rate: {float(total_successes) / float(total_episodes)}\n")
         log_file.flush()
@@ -293,7 +293,7 @@ def eval_libero(cfg: GenerateConfig) -> None:
         )
         wandb.save(local_log_filepath)
 
-    np.save(os.path.join(cfg.local_log_dir, "tasks_success_list.npy"), np.array(tasks_success_list))
+    np.save(os.path.join(cfg.local_log_dir, f"tasks_success_list_{cfg.task_suite_name}.npy"), np.array(tasks_success_list))
 
 
 if __name__ == "__main__":
